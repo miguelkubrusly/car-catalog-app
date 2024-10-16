cbf17327a66da6a81120f09393a011265313d4f8
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-16 16:25:57 -0300

    fix(list): fix confusion with use Selector

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,5 +1,43 @@
+import { ReducerCreators, Reducer, EnhancedStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store/store";
+// import {cars} from "../store/store"
+
+//TODO typing
+
+type CarListSelector = {
+  data: Car[];
+  searchTerm: string;
+};
+
 function CarList() {
-  return <div>car list</div>;
+  const { data, searchTerm }: CarListSelector = useSelector(
+    (state: any) => state.cars
+  );
+  const dispatch = useDispatch();
+
+  // const handleCarDelete = (event: React.MouseEvent<HTMLButtonElement>) => {};
+  // dispatch(removeCar());
+
+  const renderedList = data.map((car: Car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button className="button is-danger" /* onClick={handleCarDelete} */>
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedList}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
