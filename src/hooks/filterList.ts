e0a8188a1f08ad99889f9b546b691655eace44ec
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-17 17:58:08 -0300

    refactor(hooks): types + filterList ignore case

--- src/hooks/filterList.ts
+++ src/hooks/filterList.ts
@@ -1,5 +1,16 @@
-function filterList(data: any[], searchTerm: string, method: string) {
-  return data.filter((item) => item[method].includes(searchTerm));
+function filterList<T extends Record<string, any>>(
+  data: T[],
+  searchTerm: string,
+  method: keyof T
+): T[] {
+  return data.filter((item) => {
+    if (typeof item[method] === "string") {
+      const lowerValue = item[method].toLocaleLowerCase();
+      const lowerSearchTerm = searchTerm.toLocaleLowerCase();
+      return lowerValue.includes(lowerSearchTerm);
+    }
+    return false;
+  });
 }
 
 export default filterList;
