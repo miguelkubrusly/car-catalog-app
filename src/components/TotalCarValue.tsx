ed635872035b08f252d343b6d06ef34f6a6ebbbc
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-17 17:39:10 -0300

    feat(list): add dynamic total value
    -made by myself
    -updates value using search term
    -use custom hook to filter list
    -i think there is a better way of not repeat code

--- src/components/TotalCarValue.tsx
+++ src/components/TotalCarValue.tsx
@@ -1,5 +1,14 @@
+import { useSelector } from "react-redux";
+import filterList from "../hooks/filterList";
+
 function TotalCarValue() {
-  return <div>total car value</div>;
+  const { data, searchTerm } = useSelector((state: RootState) => state.cars);
+
+  const filteredList = filterList(data, searchTerm, "name");
+  const costs: number[] = filteredList.map((car) => car.cost);
+  const totalValue: number = costs.reduce((total, cost) => total + cost, 0);
+
+  return <div>{totalValue}</div>;
 }
 
 export default TotalCarValue;
